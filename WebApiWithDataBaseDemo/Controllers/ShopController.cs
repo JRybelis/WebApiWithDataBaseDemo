e48f6cbc38c7a0f001387c87d2014705341f47dd
JRybelis <72306551+JRybelis@users.noreply.github.com>
2021-06-20 22:39:05 +0300

    ShopController added

---
+++ WebApiWithDataBaseDemo/Controllers/ShopController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApiWithDataBaseDemo.Data;
+
+namespace WebApiWithDataBaseDemo.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ShopController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public ShopController(DataContext context)
+        {
+            _context = context ?? throw new ArgumentNullException(nameof(context));
+        }
+    }
+}
