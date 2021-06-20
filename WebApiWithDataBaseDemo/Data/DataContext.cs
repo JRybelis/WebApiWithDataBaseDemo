81caff712686e40c32ef69434fcedc1896bd406e
JRybelis <72306551+JRybelis@users.noreply.github.com>
2021-06-20 17:39:30 +0300

    DataContext & ShopItem class setup

---
+++ WebApiWithDataBaseDemo/Data/DataContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebApiWithDataBaseDemo.Models;
+
+namespace WebApiWithDataBaseDemo.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        public DbSet<ShopItem> ShopItems { get; set; }
+    }
+}
---
+++ WebApiWithDataBaseDemo/Models/ShopItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApiWithDataBaseDemo.Models
+{
+    public class ShopItem
+    {
+        public int PrimaryKey { get; set; }
+        public string Name { get; set; }
+    }
+}
