5d4595270337c5891217a29e369a9ef8f502c3bf
JRybelis <72306551+JRybelis@users.noreply.github.com>
2021-06-20 21:30:24 +0300

    [Key] attribute added to PrimaryKey prop to identify it as primary

--- WebApiWithDataBaseDemo/Models/ShopItem.cs
+++ WebApiWithDataBaseDemo/Models/ShopItem.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,7 @@ namespace WebApiWithDataBaseDemo.Models
 {
     public class ShopItem
     {
+        [Key]
         public int PrimaryKey { get; set; }
         public string Name { get; set; }
     }
