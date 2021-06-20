8c842eb18175a5acb3b83152d304f21a05a3ba41
JRybelis <72306551+JRybelis@users.noreply.github.com>
2021-06-20 17:44:41 +0300

    ShopItemController added. demo controller deleted

---
+++ WebApiWithDataBaseDemo/Controllers/ShopItemController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApiWithDataBaseDemo.Data;
+using WebApiWithDataBaseDemo.Models;
+
+namespace WebApiWithDataBaseDemo.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ShopItemController
+    {
+        private readonly DataContext _context;
+
+        public ShopItemController(DataContext context)
+        {
+            _context = context ?? throw new ArgumentNullException(nameof(context));
+        }
+
+
+        [HttpGet]
+        public List<ShopItem> GetAll()
+        {
+            return _context.ShopItems.ToList();
+        }
+
+        [HttpPost]
+        public void Post(ShopItem item)
+        {
+            _context.Add(item);
+            _context.SaveChanges();
+        }
+    }
+}
